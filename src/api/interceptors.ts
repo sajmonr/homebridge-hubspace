595cdbaba343d0dd5fed7b0140f274aac093db98
Adam Simonicek <adam@highmatch.com>
2023-02-15 16:37:59 -0500

    Added more comments

--- src/api/interceptors.ts
+++ src/api/interceptors.ts
@@ -1,6 +1,11 @@
 import { InternalAxiosRequestConfig } from 'axios';
 import { TokenService } from '../services/token.service';
 
+/**
+ * Adds a Bearer token to the request
+ * @param config Axios request configuration
+ * @returns Config with Bearer token
+ */
 export async function addBearerToken(config: InternalAxiosRequestConfig<any>): Promise<InternalAxiosRequestConfig<any>>{
     const token = await TokenService.instance.getToken();
 
